4eb0f3269725b14954ba39e91791330518697415
Dillon Benson <dibenso93@gmail.com>
2020-12-13 18:07:45 -0500

    get state from redux in Community and Home component and render a name

--- components/AppNavigation.js
+++ components/AppNavigation.js
@@ -1,46 +1,46 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Home from './Home';
-import Community from './Community';
+import React from "react";
+// import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Home from "./Home";
+import Community from "./Community";
 
 const Tab = createBottomTabNavigator();
 
-export default function AppNavigation( props ) {
-
-    const displayName = props.route.params.displayName;
-
-    return(
-        <NavigationContainer independent={true}>
-            <Tab.Navigator tabBarOptions={{
-                activeTintColor: '#9c2c98',
-                inactiveTintColor: 'gray',
-                style: { borderTopColor: '#9c2c98', borderWidth: 1, backgroundColor: 'black'}
-            }}>
-                <Tab.Screen name="Home" options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="home" color={color} size={size} />  
-                    )
-                }}>
-                    {props => <Home {...props} displayName={displayName}/>}
-                </Tab.Screen>
-                <Tab.Screen name="Community" options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="account-group" color={color} size={size} />  
-                    )
-                }}>
-                    {props => <Community {...props} displayName={displayName}/>}
-                </Tab.Screen>
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
-}
-
+/*
 const tabNavigatorStyles = StyleSheet.create({
-    navigator: {
-        borderWidth: 1,
-        borderColor: '#9c2c98'
-    }
-})
+  navigator: {
+    borderWidth: 1,
+    borderColor: "#9c2c98"
+  }
+});
+*/
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer independent>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: "#9c2c98",
+          inactiveTintColor: "gray",
+          style: { borderTopColor: "#9c2c98", borderWidth: 1, backgroundColor: "black" }
+        }}>
+        <Tab.Screen
+          name="Home"
+          options={{
+            tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home" color={color} size={size} />
+          }}>
+          {() => <Home />}
+        </Tab.Screen>
+        <Tab.Screen
+          name="Community"
+          options={{
+            tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="account-group" color={color} size={size} />
+          }}>
+          {() => <Community />}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
--- components/Community.js
+++ components/Community.js
@@ -1,85 +1,84 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, Image, View, SafeAreaView } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-const icon = require('../assets/fmIcon.jpg');
-
-export default function Community( props ) {
-
-    const name = props.displayName;
-
-    return(
-        <View style={ communityStyles.container }>
-            <SafeAreaView>
-            <View style={ communityStyles.header }>
-                <View style={{ flex: 1 }}>
-                <Text style={ communityStyles.name }>
-                        {name}
-                    </Text>
-                <Text style={ communityStyles.rank }>
-                    Rank: 100
-                </Text>
-                </View>
-            </View>
-            <LinearGradient 
-                style={ communityStyles.headerShadow }
-                colors={['black', '#9c2c98']}>
-                    <Text style={{ color: 'azure'}}>
-                        Community Screen
-                    </Text>
-                </LinearGradient>
-            </SafeAreaView>
-        </View>
-    );
-}
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { connect } from "react-redux";
 
 const communityStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'black',
-    },
-    header: {
-        height: 80,
-        backgroundColor: 'black',
-        flexDirection: 'row'
-    },
-    headerShadow: {
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    hText: {
-        color: 'azure', 
-        textAlign: 'center',
-        marginTop: 30,
-        fontSize: 20,
-        fontFamily: 'Staatliches_400Regular'
-    },
-    textInput: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#333',
-        color: 'azure',
-        fontSize: 20,
-        marginTop: 30,
-        marginLeft: 5,
-        marginRight: 5,
-        paddingBottom: 10,
-        fontFamily: 'Staatliches_400Regular'
-    },
-    logo: {
-        height: 50,
-        width: 50,
-        justifyContent: 'flex-start',
-        marginLeft: 10
-    },
-    name: {
-        fontFamily: 'Staatliches_400Regular',
-        color: 'azure',
-        fontSize: 30,
-        justifyContent: 'flex-start',
-    },
-    rank: {
-        fontFamily: 'Staatliches_400Regular',
-        color: '#9c2c98',
-        fontSize: 20,
-        justifyContent: 'flex-end',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  header: {
+    height: 80,
+    backgroundColor: "black",
+    flexDirection: "row"
+  },
+  headerShadow: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  hText: {
+    color: "azure",
+    textAlign: "center",
+    marginTop: 30,
+    fontSize: 20,
+    fontFamily: "Staatliches_400Regular"
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#333",
+    color: "azure",
+    fontSize: 20,
+    marginTop: 30,
+    marginLeft: 5,
+    marginRight: 5,
+    paddingBottom: 10,
+    fontFamily: "Staatliches_400Regular"
+  },
+  logo: {
+    height: 50,
+    width: 50,
+    justifyContent: "flex-start",
+    marginLeft: 10
+  },
+  name: {
+    fontFamily: "Staatliches_400Regular",
+    color: "azure",
+    fontSize: 30,
+    justifyContent: "flex-start"
+  },
+  rank: {
+    fontFamily: "Staatliches_400Regular",
+    color: "#9c2c98",
+    fontSize: 20,
+    justifyContent: "flex-end"
+  }
 });
+
+function Community(props) {
+  const { displayName, email } = props;
+  const name = displayName === "" ? email : displayName;
+
+  return (
+    <View style={communityStyles.container}>
+      <SafeAreaView>
+        <View style={communityStyles.header}>
+          <View style={{ flex: 1 }}>
+            <Text style={communityStyles.name}>{name}</Text>
+            <Text style={communityStyles.rank}>Rank: 100</Text>
+          </View>
+        </View>
+        <LinearGradient style={communityStyles.headerShadow} colors={["black", "#9c2c98"]}>
+          <Text style={{ color: "azure" }}>Community Screen</Text>
+        </LinearGradient>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const mapStateToProps = state => {
+  const { displayName, email } = state.currentUser;
+  return { displayName, email };
+};
+
+export default connect(mapStateToProps)(Community);
