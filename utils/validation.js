750e6817b5d37b2789eaf75dc33b5b124b9fa28e
Dillon Benson <dibenso93@gmail.com>
2020-12-15 15:36:18 -0500

    validate sign up form with formik and yup and display validation errors to user

--- utils/validation.js
+++ utils/validation.js
@@ -1,18 +1,23 @@
-export default function validPassword(password, confirmPassword)  {
-    if (password === confirmPassword && password.length >= 6 && confirmPassword >= 6) { return true; }
-    return false;
-}
+import * as Yup from "yup";
 
-export default function validFullName(fullName) {
-    if (fullName.length > 1) { return true; }
-    return false;
-}
+const SignupSchema = Yup.object().shape({
+  fullName: Yup.string()
+    .min(2, "Full Name Should Be At Least 2 Characters")
+    .max(50, "Full Name Should Be No More Than 50 Characters")
+    .required("Full Name Is Required"),
+  displayName: Yup.string()
+    .min(2, "Display Name Should Be At Least 2 Characters")
+    .max(50, "Display Name Should Be No More Than 2 Characters")
+    .required("Display Name Is Required"),
+  email: Yup.string().email("Email Is Invalid").required("Email Is Required"),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number And One Special Case Character"
+    )
+    .max(128, "Password Should Be No More Than 128 Characters")
+    .required("Password Is Required"),
+  confirmPassword: Yup.string().oneOf([Yup.ref("password"), null], "Passwords Must Match")
+});
 
-export default function validDisplayName(displayName) {
-    if (displayName.length >= 1) { return true; }
-    return false;
-}
-
-export default function validEmail(email) {
-    return(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email));
-}
+export default SignupSchema;
