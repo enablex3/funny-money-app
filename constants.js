042e6defb2d5d4b120362aa3760cb4b8bfa6b857
Dillon Benson <dibenso93@gmail.com>
2020-12-18 15:54:13 -0500

    dynamically assign HOST IP to HOST constant during `npm start`

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-// change HOST to your private IP
 export const HOST = "http://192.168.1.162";
+// KEEP HOST (LINE ABOVE) WHERE IT IS, ON LINE 1 OF THIS FILE.
 export const ENDPOINT_URL = `${HOST}:8080/api`;
 export const APP_NAME = "Funny Money";
