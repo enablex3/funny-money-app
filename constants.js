e7fc17a85b635541764a6cee4cd4888c46c3ed33
Dillon Benson <dibenso93@gmail.com>
2020-12-24 17:58:40 -0500

    fix errors for react-native-table-component on web

--- constants.js
+++ constants.js
@@ -1,4 +1,3 @@
-export const HOST = "http://192.168.1.239";
-// KEEP HOST (LINE ABOVE) WHERE IT IS, ON LINE 1 OF THIS FILE.
+export const HOST = "http://192.168.1.251";
 export const ENDPOINT_URL = `${HOST}:8080/api`;
 export const APP_NAME = "Funny Money";
