79a7dd31afa691a5415de12f7b25f132b98bc650
Dillon Benson <dibenso93@gmail.com>
2021-01-10 00:42:59 -0500

    fix prediction time left table column

--- components/Predictions/LatestPredictions.js
+++ components/Predictions/LatestPredictions.js
@@ -1,54 +1,58 @@
 import React from "react";
 import { StyleSheet, View, Text, Alert } from "react-native";
 import { Table, Row, Rows } from "react-native-table-component";
-import { jsonToArray } from "../../utils/jsonToArray";
-import { getDiff } from "../../utils/DateDifference";
 import { connect } from "react-redux";
+import { jsonToArray } from "../../utils/jsonToArray";
+import getDiff from "../../utils/DateDifference";
 
 const tableStyles = StyleSheet.create({
-    container: { flex: 1, backgroundColor: 'black', marginTop: 10 },
-    head: { height: 40, backgroundColor: '#9c2c98' },
-    column: { color: "azure", fontSize: 20, fontFamily: "Staatliches_400Regular"},
-    text: { margin: 6, color: "azure" },
-    row: { color: "azure", fontSize: 20 }
-  });
+  container: { flex: 1, backgroundColor: "black", marginTop: 10 },
+  head: { height: 40, backgroundColor: "#9c2c98" },
+  column: { color: "azure", fontSize: 20, fontFamily: "Staatliches_400Regular" },
+  text: { margin: 6, color: "azure" },
+  row: { color: "azure", fontSize: 20 }
+});
 
-function LatestPredictions(props) {
-    const { newPredictions } = props;
-    const newPredictionsObject = jsonToArray(newPredictions);
-    const tableHeaders = [
-        <Text style={tableStyles.column}>Name</Text>, 
-        <Text style={tableStyles.column}>Time Left</Text>,
-        <Text style={tableStyles.column}>Current Price</Text>
-    ];
-    const tableData = newPredictionsObject.map(prediction =>
-        [
-            <Text style={tableStyles.row} onPress={() => showDetails(prediction)}>{prediction.name}</Text>, 
-            <Text style={tableStyles.row} onPress={() => showDetails(prediction)}>{getDiff(prediction.value.date)}</Text>,
-            <Text style={tableStyles.row} onPress={() => showDetails(prediction)}>${prediction.value.price}</Text>
-        ]
-    );
-
-    const showDetails = (prediction) => {
-        Alert.alert("Name: " + prediction.name +
-                    "\nDate: " + prediction.value.date + 
-                    "\nPrice: " + prediction.value.price + 
-                    "\nType: " + prediction.value.type);
-    }
-
-    return(
-        <View style={tableStyles.container}>
-        <Table borderStyle={{borderWidth: 2, borderColor: 'transparent', borderBottomWidth: 2, borderBottomColor: "azure"}}>
-          <Row data={tableHeaders} style={tableStyles.head} textStyle={tableStyles.text}/>
-          <Rows data={tableData} textStyle={tableStyles.text} />
-        </Table>
-      </View>
-    )
+const showDetails = prediction => {
+  Alert.alert(
+    `Name: ${prediction.name}\nDate: ${prediction.value.date}\nPrice: ${prediction.value.price}\nType: ${prediction.value.type}`
+  );
 };
 
+function LatestPredictions(props) {
+  const { newPredictions } = props;
+  const newPredictionsObject = jsonToArray(newPredictions);
+  const tableHeaders = [
+    <Text style={tableStyles.column}>Name</Text>,
+    <Text style={tableStyles.column}>Time Left</Text>,
+    <Text style={tableStyles.column}>Current Price</Text>
+  ];
+  const tableData = newPredictionsObject.map(prediction => [
+    <Text style={tableStyles.row} onPress={() => showDetails(prediction)}>
+      {prediction.name}
+    </Text>,
+    <Text style={tableStyles.row} onPress={() => showDetails(prediction)}>
+      {getDiff(prediction.value.date)}
+    </Text>,
+    <Text style={tableStyles.row} onPress={() => showDetails(prediction)}>
+      {`$ ${prediction.value.price}`}
+    </Text>
+  ]);
+
+  return (
+    <View style={tableStyles.container}>
+      <Table
+        borderStyle={{ borderWidth: 2, borderColor: "transparent", borderBottomWidth: 2, borderBottomColor: "azure" }}>
+        <Row data={tableHeaders} style={tableStyles.head} textStyle={tableStyles.text} />
+        <Rows data={tableData} textStyle={tableStyles.text} />
+      </Table>
+    </View>
+  );
+}
+
 const mapStateToProps = state => {
-    const { newPredictions } = state.currentUser;
-    return { newPredictions };
+  const { newPredictions } = state.currentUser;
+  return { newPredictions };
 };
 
 export default connect(mapStateToProps)(LatestPredictions);
--- utils/DateDifference.js
+++ utils/DateDifference.js
@@ -1,13 +1,11 @@
-var DateDiff = require('date-diff');
+const DateDiff = require("date-diff");
 
-export const getDiff = (date) => {
-    const currentDate = new Date();
-    console.log(currentDate);
-    const month = Number(date.split("-")[0]);
-    const day = Number(date.split("-")[1]);
-    const year = Number(date.split("-")[2]);
-    const predictionDate = new Date(year, day, month);
-    console.log(predictionDate);
-    var diff = new DateDiff(predictionDate, currentDate);
-    return diff.days().toString() + " days";
+const getDiff = date => {
+  const currentDate = new Date();
+  const [month, day, year] = date.split("-").map(value => Number(value));
+  const predictionDate = new Date(year, month - 1, day);
+  const diff = new DateDiff(predictionDate, currentDate);
+  return `${diff.days().toString()} days`;
 };
+
+export default getDiff;
